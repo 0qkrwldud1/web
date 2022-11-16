a1cf6ead0f3c46a4373e4a626f1fc3e0b51b8290
0qkrwldud1 <0qkrwldud1@naver.com>
2022-11-16 23:01:09 +0900

    bscoding6916

--- colors.js
+++ colors.js
@@ -6,52 +6,30 @@ var Body = {
       $('body').css('backgroundColor', color);
    }
   }
+
 var Links = {
     setColor: function(color) {
-      $('a').css('color', color);
-     }
-     }
-   
-   // function BodySetBackgroudSetColor(color) {
-   // document.querySelector('body').style.backgroundColor = color;
-   // }
-   
-   // function BodySetColor(color) {
-   // document.querySelector('body').style.color = color;
-   // }
-   
-   // function setColor(color) {
-   // var alist = document.querySelectorAll('a');
-   // var i = 0;
-   // while ( i < alist.length) {
-   // 		alist[i].style.color = color;
-   // 		console.log(alist[i]);
-   // 		i = i + 1;
-   // 	}
-   // }
-   
-     function nightDayHandler(self) {
-           var target = document.querySelector('body');
-           if (self.value === 'night') {
-               //target.style.backgroundColor = 'black';
-               //target.style.color = 'white';
-               // BodySetColor('white')
-               // BodySetBackgroudSetColor('black')
-               // setColor('powderblue')
-               Body.setBackgroundColor('black')
-               Body.setColor('white')
-               self.value = 'day' 
-               Links.setColor('powderblue')
-               
-           } else {
-               //target.style.backgroundColor = 'white';
-               //target.style.color = 'black';
-               // BodySetColor('black')
-               // BodySetBackgroudSetColor('white')
-               // setColor('blue')
-               Body.setBackgroundColor('white')
-               Body.setColor('black')
-               self.value = 'night'
-               Links.setColor('blue')
-               }
-   }
+      var alist = document.querySelectorAll('a');
+      var i = 0;
+      while(i < alist.length) {
+      alist[i].style.color = color;
+      i = i + 1;
+    }
+    }
+  }
+  
+function nightDayHandler(self) {
+  var target = document.querySelector('body');
+  if (self.value === 'night') {
+    Body.setBackgroundColor('black');
+    Body.setColor('white');
+    Links.setColor('powderblue') ;
+    self.value = 'day' 
+    } 
+  else {
+    Body.setBackgroundColor('white');
+    Body.setColor('black');
+    Links.setColor('blue') ;
+    self.value = 'night'
+    }
+  }
